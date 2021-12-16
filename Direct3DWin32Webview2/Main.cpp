8e4eac456d4789c1b299a8b4b9f8371c07b2a646
DEZEIRAUD Gaëtan <gaetan@dezeiraud.com>
2021-12-16 14:30:54 +0100

    initial commit

---
+++ Direct3DWin32Webview2/Main.cpp
@@ -0,0 +1,343 @@
+﻿//
+// Main.cpp
+//
+
+#include "pch.h"
+#include "Game.h"
+#include <WebView2EnvironmentOptions.h>
+
+using namespace DirectX;
+
+#ifdef __clang__
+#pragma clang diagnostic ignored "-Wcovered-switch-default"
+#pragma clang diagnostic ignored "-Wswitch-enum"
+#endif
+
+#pragma warning(disable : 4061)
+
+namespace
+{
+    std::unique_ptr<Game> g_game;
+}
+
+LPCWSTR g_szAppName = L"Direct3DWin32Webview2";
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void ExitGame() noexcept;
+
+// Indicates to hybrid graphics systems to prefer the discrete part by default
+extern "C"
+{
+    __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
+static wil::com_ptr<ICoreWebView2> webviewWindow;
+static wil::com_ptr<ICoreWebView2Controller> webviewController;
+
+// Entry point
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    if (!XMVerifyCPUSupport())
+        return 1;
+
+    // 0x80010106 "Cannot change thread mode after it is set." with WebView2
+    /*
+    HRESULT hr = CoInitializeEx(nullptr, COINITBASE_MULTITHREADED);
+    if (FAILED(hr))
+        return 1;
+    */
+
+    HWND hwnd;
+    g_game = std::make_unique<Game>();
+
+    // Register class and create window
+    {
+        // Register class
+        WNDCLASSEXW wcex = {};
+        wcex.cbSize = sizeof(WNDCLASSEXW);
+        wcex.style = CS_HREDRAW | CS_VREDRAW;
+        wcex.lpfnWndProc = WndProc;
+        wcex.hInstance = hInstance;
+        wcex.hIcon = LoadIconW(hInstance, L"IDI_ICON");
+        wcex.hCursor = LoadCursorW(nullptr, IDC_ARROW);
+        wcex.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+        wcex.lpszClassName = L"Direct3DWin32Webview2WindowClass";
+        wcex.hIconSm = LoadIconW(wcex.hInstance, L"IDI_ICON");
+        if (!RegisterClassExW(&wcex))
+            return 1;
+
+        // Create window
+        int w, h;
+        g_game->GetDefaultSize(w, h);
+
+        RECT rc = { 0, 0, static_cast<LONG>(w), static_cast<LONG>(h) };
+
+        AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+        hwnd = CreateWindowExW(0, L"Direct3DWin32Webview2WindowClass", g_szAppName, WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
+            nullptr);
+        // TODO: Change to CreateWindowExW(WS_EX_TOPMOST, L"Direct3DWin32Webview2WindowClass", g_szAppName, WS_POPUP,
+        // to default to fullscreen.
+
+        if (!hwnd)
+            return 1;
+
+        ShowWindow(hwnd, nCmdShow);
+        UpdateWindow(hwnd);
+        // TODO: Change nCmdShow to SW_SHOWMAXIMIZED to default to fullscreen.
+
+        SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(g_game.get()) );
+
+        GetClientRect(hwnd, &rc);
+
+        g_game->Initialize(hwnd, rc.right - rc.left, rc.bottom - rc.top);
+
+        // Setup WebView2
+        CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+            Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+                [hwnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+
+                    // Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
+                    env->CreateCoreWebView2Controller(hwnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+                        [hwnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+                            if (controller != nullptr) {
+                                webviewController = controller;
+                                webviewController->get_CoreWebView2(&webviewWindow);
+                            }
+
+                            // Add a few settings for the webview
+                            // The demo step is redundant since the values are the default settings
+                            ICoreWebView2Settings* Settings;
+                            webviewWindow->get_Settings(&Settings);
+                            Settings->put_IsScriptEnabled(TRUE);
+                            Settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+                            Settings->put_IsWebMessageEnabled(TRUE);
+
+                            // Resize the WebView2 control to fit the bounds of the parent window
+                            RECT bounds;
+                            GetClientRect(hwnd, &bounds);
+                            webviewController->put_Bounds(bounds);
+
+                            // Transparent background
+                            COREWEBVIEW2_COLOR wvColor;
+                            wvColor.R = 255;
+                            wvColor.G = 255;
+                            wvColor.B = 255;
+                            wvColor.A = 0;
+
+                            wil::com_ptr<ICoreWebView2Controller2> controller2 = webviewController.query<ICoreWebView2Controller2>();
+                            controller2->put_DefaultBackgroundColor(wvColor);
+
+                            // Schedule an async task to navigate
+                            const wchar_t* HTMLStr =
+                                L"<!DOCTYPE html>"
+                                L"<html lang=\"en\">"
+                                L"<head>"
+                                L"  <meta charset=\"utf-8\">"
+                                L"  <title>WebView2</title>"
+                                L"</head>"
+                                L"<body>"
+                                L"  <h1>Hello, 🌍!</h1>"
+                                L"  <button>Close</button>"
+                                L"</body>"
+                                L"</html>";
+
+                            webviewWindow->NavigateToString(HTMLStr);
+
+                            // 4 - Navigation events
+
+                            // 5 - Scripting
+
+                            // 6 - Communication between host and web content
+
+                            return S_OK;
+                        }).Get());
+                    return S_OK;
+                }).Get());
+    }
+
+    // Main message loop
+    MSG msg = {};
+    while (WM_QUIT != msg.message)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            g_game->Tick();
+        }
+    }
+
+    g_game.reset();
+
+    CoUninitialize();
+
+    return static_cast<int>(msg.wParam);
+}
+
+// Windows procedure
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static bool s_in_sizemove = false;
+    static bool s_in_suspend = false;
+    static bool s_minimized = false;
+    static bool s_fullscreen = false;
+    // TODO: Set s_fullscreen to true if defaulting to fullscreen.
+
+    auto game = reinterpret_cast<Game*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+
+    switch (message)
+    {
+    case WM_PAINT:
+        if (s_in_sizemove && game)
+        {
+            game->Tick();
+        }
+        else
+        {
+            PAINTSTRUCT ps;
+            (void)BeginPaint(hWnd, &ps);
+            EndPaint(hWnd, &ps);
+        }
+        break;
+
+    case WM_SIZE:
+        if (wParam == SIZE_MINIMIZED)
+        {
+            if (!s_minimized)
+            {
+                s_minimized = true;
+                if (!s_in_suspend && game)
+                    game->OnSuspending();
+                s_in_suspend = true;
+            }
+        }
+        else if (s_minimized)
+        {
+            s_minimized = false;
+            if (s_in_suspend && game)
+                game->OnResuming();
+            s_in_suspend = false;
+        }
+        else if (!s_in_sizemove && game)
+        {
+            game->OnWindowSizeChanged(LOWORD(lParam), HIWORD(lParam));
+        }
+        break;
+
+    case WM_ENTERSIZEMOVE:
+        s_in_sizemove = true;
+        break;
+
+    case WM_EXITSIZEMOVE:
+        s_in_sizemove = false;
+        if (game)
+        {
+            RECT rc;
+            GetClientRect(hWnd, &rc);
+
+            game->OnWindowSizeChanged(rc.right - rc.left, rc.bottom - rc.top);
+        }
+        break;
+
+    case WM_GETMINMAXINFO:
+        if (lParam)
+        {
+            auto info = reinterpret_cast<MINMAXINFO*>(lParam);
+            info->ptMinTrackSize.x = 320;
+            info->ptMinTrackSize.y = 200;
+        }
+        break;
+
+    case WM_ACTIVATEAPP:
+        if (game)
+        {
+            if (wParam)
+            {
+                game->OnActivated();
+            }
+            else
+            {
+                game->OnDeactivated();
+            }
+        }
+        break;
+
+    case WM_POWERBROADCAST:
+        switch (wParam)
+        {
+        case PBT_APMQUERYSUSPEND:
+            if (!s_in_suspend && game)
+                game->OnSuspending();
+            s_in_suspend = true;
+            return TRUE;
+
+        case PBT_APMRESUMESUSPEND:
+            if (!s_minimized)
+            {
+                if (s_in_suspend && game)
+                    game->OnResuming();
+                s_in_suspend = false;
+            }
+            return TRUE;
+        }
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    case WM_SYSKEYDOWN:
+        if (wParam == VK_RETURN && (lParam & 0x60000000) == 0x20000000)
+        {
+            // Implements the classic ALT+ENTER fullscreen toggle
+            if (s_fullscreen)
+            {
+                SetWindowLongPtr(hWnd, GWL_STYLE, WS_OVERLAPPEDWINDOW);
+                SetWindowLongPtr(hWnd, GWL_EXSTYLE, 0);
+
+                int width = 800;
+                int height = 600;
+                if (game)
+                    game->GetDefaultSize(width, height);
+
+                ShowWindow(hWnd, SW_SHOWNORMAL);
+
+                SetWindowPos(hWnd, HWND_TOP, 0, 0, width, height, SWP_NOMOVE | SWP_NOZORDER | SWP_FRAMECHANGED);
+            }
+            else
+            {
+                SetWindowLongPtr(hWnd, GWL_STYLE, WS_POPUP);
+                SetWindowLongPtr(hWnd, GWL_EXSTYLE, WS_EX_TOPMOST);
+
+                SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+                ShowWindow(hWnd, SW_SHOWMAXIMIZED);
+            }
+
+            s_fullscreen = !s_fullscreen;
+        }
+        break;
+
+    case WM_MENUCHAR:
+        // A menu is active and the user presses a key that does not correspond
+        // to any mnemonic or accelerator key. Ignore so we don't produce an error beep.
+        return MAKELRESULT(0, MNC_CLOSE);
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+// Exit helper
+void ExitGame() noexcept
+{
+    PostQuitMessage(0);
+}
